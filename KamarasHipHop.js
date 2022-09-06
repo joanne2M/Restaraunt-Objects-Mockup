226f54e2596ec546ade94065c4c3a8b258714026
Kamara Daughtry <97330592+joanne2M@users.noreply.github.com>
2022-09-05 21:29:23 -0400

    Create KamarasHipHop.js

---
+++ KamarasHipHop.js
@@ -0,0 +1,109 @@
+const menu = {
+  
+  Hip Hop Brunch: {
+    Chicken and Waffles: {
+      price: 8.75,
+    },
+    Money Scrambled Eggs: {
+      price: 5.77,
+    },
+    Sausage and Bacon: {
+      price: 8.99,
+    },
+   Lebron James Meal: {
+      includes: Panackes, Waffles, Sausauge, Bacon and Orange Juice
+      price: 15.99,
+    },
+    
+  :Breakfast {
+ 
+  Muffins (Anykind): {
+      price: 2.13,
+    },
+    
+   Energy Bars: {
+      price: 5,89,
+    },
+    
+    Oatmeal{
+      price: 4.99,
+    },
+  
+  Grits (No Sugar): {
+      price: 7.99,
+      
+ :Coffee and Tea {
+ 
+  Black Tea {
+      price: 7.19,
+    },
+    
+   No More Coughing Tea: {
+      price: 4.99,
+    },
+    
+   Pumpkin Coffee {
+      price: 11.99,
+    },
+  
+  Black Coffee: {
+      price: 6.99,
+
+:Desert {
+ 
+  Cotton Candy Cake {
+      price: 11.99
+    },
+    
+   Sherbert: {
+      price: 8.99,
+    },
+    
+   Birthday Cake {
+      price: 4.99,
+    },
+  
+ :Specials {
+ 
+  I Got 5 On It {
+      price: 5.99 (Two Menue Items)
+    },
+    
+   Keep it 7 {
+      price: 6.99,(Two Drinks)
+    },
+    
+   Cookie Monster {
+      price: 5.99,
+    },
+  
+ :Soul Food {
+ 
+  Turkey, Dressing, Collard Greens, Yams {
+      price: 17.99 
+    },
+    
+   Peach Cobblar & Fried Chicken {
+      price 16.99 
+    },
+    
+   Family Meal (Ask Cashier) {
+      price: 54.99,
+    },
+  
+ :Quick Food {
+ 
+  2 Pieces of Chicken and 1 Side {
+      price: 13.99
+    },
+    
+  Cofeed, Bagel, Protein {
+      price 14.99
+    },
+    
+   Customers Choice {
+      price:15.99 (3 Items)
+  
+    },
+  },
+};
